d6d9928eba5eda7b121ee988acee58c12135a028
Maciej Sawicki <maciej_ext@helloclue.com>
2022-08-24 12:29:36 +0200

    feat: release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,25 @@
 const sh = require('shelljs');
+const fs = require('fs');
 
-sh.exec('npm version patch');
+const manifestPath = 'src/chrome/manifest.json';
+
+const updatedVersion = updatePackageVersion();
+updateManifestVersion(updatedVersion);
+tagAndCommit(updatedVersion);
+
+function updatePackageVersion() {
+  const version = sh.exec('npm --no-git-tag-version version patch').stdout;
+  return version.slice(1).trim();
+}
+
+function updateManifestVersion(version) {
+  const manifestJson = JSON.parse(fs.readFileSync(manifestPath));
+  manifestJson.version = version;
+  fs.writeFileSync(manifestPath, JSON.stringify(manifestJson, null, 2));
+}
+
+function tagAndCommit(version) {
+  sh.exec('git add *');
+  sh.exec(`git commit -m 'release: v${version}'`);
+  sh.exec(`git tag v${version}`);
+}
